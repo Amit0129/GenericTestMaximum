06601c6159852122b0a26fe168739e23c25a1755
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-29 15:13:36 +0530

    [Amit] Given Three integer find the Largest

---
+++ GenericTestMaximum/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericTestMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMaximum testMaximum = new TestMaximum();
+            int maxNumber = testMaximum.GetMaximumIntNumber();
+            Console.WriteLine($"The maxumum number between three integer is : {maxNumber}");
+        }
+    }
+}
---
+++ GenericTestMaximum/TestMaximum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTestMaximum
+{
+    public class TestMaximum
+    {
+        public int GetMaximumIntNumber()
+        {
+            Console.WriteLine("Eneter First Number");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Eneter Second Number");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Eneter Third Number");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0 ||
+               numOne.CompareTo(numTwo) >= 0 && numOne.CompareTo(numThree) > 0 ||
+               numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) >= 0)
+            {
+                return numOne;
+            }
+            if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0 ||
+                numTwo.CompareTo(numOne) >= 0 && numTwo.CompareTo(numThree) > 0 ||
+                numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) >= 0)
+            {
+                return numTwo;
+            }
+            if (numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) > 0 ||
+                numThree.CompareTo(numTwo) >= 0 && numThree.CompareTo(numOne) > 0 ||
+                numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) >= 0)
+            {
+                return numThree;
+            }
+            return numOne;
+
+        }
+    }
+}
