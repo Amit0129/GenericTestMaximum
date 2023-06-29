deb529968342ca4f8d79b9216d3d4f056d3d87c8
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-29 16:22:16 +0530

    [Amit] Refctoring the code to Generic method

--- GenericTestMaximum/Program.cs
+++ GenericTestMaximum/Program.cs
@@ -13,17 +13,34 @@
             switch (choice)
             {
                 case 1:
-                    
-                    int maxNumber = testMaximum.GetMaximumIntNumber();
+                    Console.WriteLine("Enter First Number");
+                    int numOne = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Second Number");
+                    int numTwo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Third Number");
+                    int numThree = Convert.ToInt32(Console.ReadLine());
+                    int maxNumber = testMaximum.GetMaximun<int>(numOne, numTwo, numThree);
                     Console.WriteLine($"The maxumum number between three integer is : {maxNumber}");
                     break;
                 case 2:
-                    float maxFloat = testMaximum.GetMaximumFloat();
+                    Console.WriteLine("Enter First float number");
+                    float floatOne = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Second float number");
+                    float floatTwo = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Third floatnumber");
+                    float floatThree = float.Parse(Console.ReadLine());
+                    float maxFloat = testMaximum.GetMaximun<float>(floatOne, floatTwo, floatThree);
                     Console.WriteLine($"The maxumum number between three float number is : {maxFloat}");
                     break;
                 case 3:
-                    string maxString = testMaximum.GetMaximumString();
-                    Console.WriteLine($"The maxumum number between three string is : {maxString}");
+                    Console.WriteLine("Enter First string");
+                    string stringOne = Console.ReadLine();
+                    Console.WriteLine("Enter Second string");
+                    string stringTwo = Console.ReadLine();
+                    Console.WriteLine("Enter Third string");
+                    string stringThree = Console.ReadLine();
+                    string maxString = testMaximum.GetMaximun<string>(stringOne, stringTwo, stringThree);
+                    Console.WriteLine($"The maxumum between three string is : {maxString}");
                     break;
                 default:
                     Console.WriteLine("Please Enter a valid choice");
--- GenericTestMaximum/TestMaximum.cs
+++ GenericTestMaximum/TestMaximum.cs
@@ -8,90 +8,30 @@ namespace GenericTestMaximum
 {
     public class TestMaximum
     {
-        public int GetMaximumIntNumber()
+        public T GetMaximun<T>(T valueOne, T valueTwo, T valueThree) where T : IComparable
         {
-            Console.WriteLine("Enter First Number");
-            int numOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Second Number");
-            int numTwo = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Third Number");
-            int numThree = Convert.ToInt32(Console.ReadLine());
-            if (numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) > 0 ||
-               numOne.CompareTo(numTwo) >= 0 && numOne.CompareTo(numThree) > 0 ||
-               numOne.CompareTo(numTwo) > 0 && numOne.CompareTo(numThree) >= 0)
+            
+            if (valueOne.CompareTo(valueTwo) > 0 && valueOne.CompareTo(valueThree) > 0 ||
+               valueOne.CompareTo(valueTwo) >= 0 && valueOne.CompareTo(valueThree) > 0 ||
+               valueOne.CompareTo(valueTwo) > 0 && valueOne.CompareTo(valueThree) >= 0)
             {
-                return numOne;
+                return valueOne;
             }
-            if (numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) > 0 ||
-                numTwo.CompareTo(numOne) >= 0 && numTwo.CompareTo(numThree) > 0 ||
-                numTwo.CompareTo(numOne) > 0 && numTwo.CompareTo(numThree) >= 0)
+            if (valueTwo.CompareTo(valueOne) > 0 && valueTwo.CompareTo(valueThree) > 0 ||
+                valueTwo.CompareTo(valueOne) >= 0 && valueTwo.CompareTo(valueThree) > 0 ||
+                valueTwo.CompareTo(valueOne) > 0 && valueTwo.CompareTo(valueThree) >= 0)
             {
-                return numTwo;
+                return valueTwo;
             }
-            if (numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) > 0 ||
-                numThree.CompareTo(numTwo) >= 0 && numThree.CompareTo(numOne) > 0 ||
-                numThree.CompareTo(numTwo) > 0 && numThree.CompareTo(numOne) >= 0)
+            if (valueThree.CompareTo(valueTwo) > 0 && valueThree.CompareTo(valueOne) > 0 ||
+                valueThree.CompareTo(valueTwo) >= 0 && valueThree.CompareTo(valueOne) > 0 ||
+                valueThree.CompareTo(valueTwo) > 0 && valueThree.CompareTo(valueOne) >= 0)
             {
-                return numThree;
+                return valueThree;
             }
-            return numOne;
+            return valueOne;
 
         }
-        public float GetMaximumFloat()
-        {
-            Console.WriteLine("Enter First float number");
-            float floatOne = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Second float number");
-            float floatTwo = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Third floatnumber");
-            float floatThree = float.Parse(Console.ReadLine());
-            if (floatOne.CompareTo(floatTwo) > 0 && floatOne.CompareTo(floatThree) > 0 ||
-               floatOne.CompareTo(floatTwo) >= 0 && floatOne.CompareTo(floatThree) > 0 ||
-               floatOne.CompareTo(floatTwo) > 0 && floatOne.CompareTo(floatThree) >= 0)
-            {
-                return floatOne;
-            }
-            if (floatTwo.CompareTo(floatOne) > 0 && floatTwo.CompareTo(floatThree) > 0 ||
-                floatTwo.CompareTo(floatOne) >= 0 && floatTwo.CompareTo(floatThree) > 0 ||
-                floatTwo.CompareTo(floatOne) > 0 && floatTwo.CompareTo(floatThree) >= 0)
-            {
-                return floatTwo;
-            }
-            if (floatThree.CompareTo(floatTwo) > 0 && floatThree.CompareTo(floatOne) > 0 ||
-                floatThree.CompareTo(floatTwo) >= 0 && floatThree.CompareTo(floatOne) > 0 ||
-                floatThree.CompareTo(floatTwo) > 0 && floatThree.CompareTo(floatOne) >= 0)
-            {
-                return floatThree;
-            }
-            return floatOne;
-        }
-        public string GetMaximumString()
-        {
-            Console.WriteLine("Enter First string");
-            string stringOne = Console.ReadLine();
-            Console.WriteLine("Enter Second string");
-            string stringTwo = Console.ReadLine();
-            Console.WriteLine("Enter Third string");
-            string stringThree = Console.ReadLine();
-            if (stringOne.CompareTo(stringTwo) > 0 && stringOne.CompareTo(stringThree) > 0 ||
-               stringOne.CompareTo(stringTwo) >= 0 && stringOne.CompareTo(stringThree) > 0 ||
-               stringOne.CompareTo(stringTwo) > 0 && stringOne.CompareTo(stringThree) >= 0)
-            {
-                return stringOne;
-            }
-            if (stringTwo.CompareTo(stringOne) > 0 && stringTwo.CompareTo(stringThree) > 0 ||
-                stringTwo.CompareTo(stringOne) >= 0 && stringTwo.CompareTo(stringThree) > 0 ||
-                stringTwo.CompareTo(stringOne) > 0 && stringTwo.CompareTo(stringThree) >= 0)
-            {
-                return stringTwo;
-            }
-            if (stringThree.CompareTo(stringTwo) > 0 && stringThree.CompareTo(stringOne) > 0 ||
-                stringThree.CompareTo(stringTwo) >= 0 && stringThree.CompareTo(stringOne) > 0 ||
-                stringThree.CompareTo(stringTwo) > 0 && stringThree.CompareTo(stringOne) >= 0)
-            {
-                return stringThree;
-            }
-            return stringOne;
-        }
+
     }
 }
